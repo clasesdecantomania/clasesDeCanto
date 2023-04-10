b45899d0a8dabcec68c2126bede6a413d270d965
clasesdecantomania <maximilianoalonso2012.ma@gmail.com>
2023-04-10 13:01:07 -0500

    cambios 10/04

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   AiOutlineMenu,
   AiOutlineHome,
   AiOutlineProject,
   AiOutlineMail,
-} from 'react-icons/ai';
-import { BsPerson } from 'react-icons/bs';
-import { GrProjects } from 'react-icons/gr';
+} from "react-icons/ai";
+import { BsPerson } from "react-icons/bs";
+import { GrProjects } from "react-icons/gr";
 
 const Sidenav = () => {
   const [nav, setNav] = useState(false);
@@ -18,71 +18,96 @@ const Sidenav = () => {
     <div>
       <AiOutlineMenu
         onClick={handleNav}
-        className='absolute top-4 right-4 z-[99] md:hidden'
+        className="absolute top-4 right-4 z-[99] md:hidden"
       />
       {nav ? (
-        <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
+        <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
           <a
-            href='#main'
-            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineHome size={20} />
-            <span className='pl-4'>Inicio</span>
+            <span className="pl-4">Inicio</span>
           </a>
-          <a onClick={handleNav}
-            href='#work'
-            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+          <a
+            onClick={handleNav}
+            href="#work"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
-            <span className='pl-4'>Experiencia</span>
+            <span className="pl-4">Experiencia</span>
           </a>
-          <a onClick={handleNav}
-            href='#projects'
-            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+          <a
+            onClick={handleNav}
+            href="#projects"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineProject size={20} />
-            <span className='pl-4'>Galeria</span>
+            <span className="pl-4">Galeria</span>
           </a>
-          <a onClick={handleNav}
-            href='#main'
-            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+          <a
+            onClick={handleNav}
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <BsPerson size={20} />
-            <span className='pl-4'>Clases</span>
+            <span className="pl-4">Clases</span>
           </a>
-          <a onClick={handleNav}
-            href='#contact'
-            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+          <a
+            onClick={handleNav}
+            href="#contact"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineMail size={20} />
-            <span className='pl-4'>Contacto</span>
+            <span className="pl-4">Contacto</span>
           </a>
         </div>
       ) : (
-        ''
+        ""
       )}
 
-     <div className='md:block hidden fixed top-[25%] z-10'>
-        <div className='flex flex-col'>
-            <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                <AiOutlineHome size={20} />
-            </a>
-            <a href='#work' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                <GrProjects size={20} />
-            </a>
-            <a href='#projects' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                <AiOutlineProject size={20} />
-            </a>
-            <a href='#main' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                <BsPerson size={20} />
-            </a>
-            <a href='#contact' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-                <AiOutlineMail size={20} />
-            </a>
+      <div className="md:block hidden fixed top-[25%] z-10">
+        <div className="flex flex-col">
+          <a
+            href="#main"
+            title="Ir a Inicio"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <AiOutlineHome size={20} />
+          </a>
+          <a
+            href="#work"
+            title="Experiencia"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <GrProjects size={20} />
+          </a>
+          <a
+            href="#projects"
+            title="Galeria de Imagenes"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <AiOutlineProject size={20} />
+          </a>
+          <a
+            href="https://drive.google.com/file/d/14quILgT7fpniOiHSn2dvw41bgPCiqRFK/view?usp=share_link"
+            title="Descargar CV"
+            target="_blank"
+            
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <BsPerson size={20} />
+          </a>
+
+          <a
+            href="#contact"
+            title="Contacto"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <AiOutlineMail size={20} />
+          </a>
         </div>
-     </div>
-
-
+      </div>
     </div>
   );
 };
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,35 +2,56 @@ import React from 'react';
 import WorkItem from './WorkItem';
 
 const data = [
-{
-year: 2022,
-title: 'Director del Coro de la Escuela Nacional de Música Carlos López Buchardo',
-duration: '1 Año',
-details:
-'Encargado de la dirección del coro y la organización de ensayos y presentaciones.',
-},
-{
-year: 2019,
-title: 'Profesor de Canto en la Escuela de Música Popular de Avellaneda',
-duration: '3 Años',
-details:
-'Impartí clases de técnica vocal y repertorio a estudiantes de diferentes edades y niveles.',
-},
-{
-year: 2016,
-title: 'Participación en la obra "Los Miserables" en el Teatro Ópera',
-duration: '6 Meses',
-details:
-'Formé parte del elenco del coro y también interpreté algunos solos en esta reconocida obra.',
-},
-{
-year: 2014,
-title: 'Participación en la ópera "La Traviata" en el Teatro Colón',
-duration: '4 Meses',
-details:
-'Formé parte del coro estable en esta prestigiosa ópera, interpretando diferentes papeles secundarios.',
-},
-];
+  {
+  year: 2022,
+  title: 'Participación en la obra "El Jorobado de París" - Cibrián-Mahler, interpretando el papel del poeta Pierre Gringore',
+  duration: 'Actualidad',
+  details:
+  'Formo parte del elenco de la obra y asumo el papel del poeta Pierre Gringore.',
+  },
+  {
+  year: 2019,
+  title: 'Docente de canto para adultos y jóvenes en el Centro Cultural Abiespaciodearte en Parque San Martin',
+  duration: 'Actualidad',
+  details:
+  'Imparto clases de técnica vocal y repertorio a estudiantes de diferentes edades y niveles para ayudarles a desarrollar su habilidad vocal.',
+  },
+  {
+  year: 2014,
+  title: 'Integrante del Coro del Conservatorio Alberto Ginastera de Morón',
+  duration: '1 Año',
+  details:
+  'Participé en distintas presentaciones del coro, incluyendo un concierto en el "Teatro Gran Rex" en octubre de 2014, lo que me permitió desarrollar mis habilidades como cantante y mejorar mi técnica vocal.',
+  },
+  {
+  year: 2013,
+  title: 'Recital como solista en el Auditorio de Belgrano',
+  duration: '1 Presentación',
+  details:
+  'Presenté un recital como solista en el Auditorio de Belgrano en el año 2013, lo que me permitió mostrar mi talento y consolidar mi experiencia como intérprete.',
+  },
+  {
+  year: 2012,
+  title: 'Recital como solista en el Museo de Bellas Artes de Moreno, Francisco Molina Campos',
+  duration: '1 Presentación',
+  details:
+  'Presenté un recital como solista en el "Museo de Bellas Artes de Moreno, Francisco Molina Campos" en el año 2012, lo que me permitió adquirir experiencia como intérprete en distintos espacios culturales.',
+  },
+  {
+  year: 2007,
+  title: 'Asistente de la profesora de técnica vocal del Coro Italiano de Merlo',
+  duration: 'Actualidad',
+  details:
+  'Colaboro en la enseñanza de la técnica vocal y en la preparación de los ensayos y presentaciones del coro, lo que me permite mejorar mis habilidades como cantante y docente.',
+  },
+  {
+  year: 2007,
+  title: 'Jefe de Cuerda de Tenor en el Coro Italiano de Merlo, perteneciente a la Sociedad Italiana de Merlo',
+  duration: 'Actualidad',
+  details:
+  'Soy el responsable de la coordinación y liderazgo de los tenores en el coro, participando en distintos eventos y viajes, y contribuyendo a la calidad musical del grupo.',
+  },
+  ];
 
 const Work = () => {
   return (
