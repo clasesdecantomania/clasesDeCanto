320b13f17c4a030e1ad17cf2910e7bf0e5cb48f4
clasesdecantomania <maximilianoalonso2012.ma@gmail.com>
2023-04-11 03:28:01 -0500

    img

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import Contact from "./components/Contact";
 import { Helmet } from "react-helmet";
 
 function App() {
-  const shareImage = "/src/assets/briancanto.webp";
+  const shareImage = "https://i.postimg.cc/SNkK631z/clases-Canto.png";
+
   const pageTitle = "Clases de Canto | Merlo";
   const pageDescription = "Aprende a cantar con Brian, profesor de canto en Merlo, Argentina. Clases personalizadas para todos los niveles y edades.";
   const pageUrl = "https://clases-de-canto.netlify.app/";
@@ -22,8 +23,8 @@ function App() {
         <meta name="description" content={pageDescription} />
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={pageDescription} />
-        <meta property="og:image" content="https://clases-de-canto.netlify.app/src/assets/briancanto.webp" />
-        <meta property="og:image:secure_url" content={shareImage} />
+        <meta property="og:image" content="https://i.postimg.cc/SNkK631z/clases-Canto.png" />
+        <meta property="og:image:secure_url" content="https://i.postimg.cc/SNkK631z/clases-Canto.png" />
         <meta property="og:url" content={pageUrl} />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
